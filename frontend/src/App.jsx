0861f9708c92be7bf84ea7020356cb0281f97c4f
Samiran Sarkar <pappu@gmail.com>
2025-12-19 20:36:38 +0530

    path resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -102,7 +102,7 @@ function App() {
           }
         />
         {/* catch all routes */}
-        <Route path='/{*any}' element={<Navigate to='/' replace />} />
+        <Route path="*" element={<Navigate to='/' replace />} />
       </Routes>
       <Toaster />
     </div>
